1be006d0f07f6b4f6fe79f61eb5908317644eac6
melexchi <melexchi@gmail.com>
2025-05-09 08:14:39 +0100

    added more codes to blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -20,15 +20,26 @@ const blogPosts = [
 
 export default function BlogPage() {
   return (
-    <div className="w-full px-2 xl:px-10 h-auto mb-5">
-      <h1>Blogs</h1>
+    <> 
+    <h1 className="pt-2 flex justify-center  md:justify-start px-2 xl:px-20">Blogs</h1>
+    <div className="w-auto px-2 xl:px-20 h-auto mb-5 border-2 border-t-0 border-gray-200 grid grid-cols-1 md:grid-cols-2 gap-5">
+      
       {blogPosts.map((blog) => (
-        <div key={blog.slug} className="blog-post flex ">
+        <div className=" pt-5 w-full grid-cols-subgrid"> 
+        <div key={blog.slug} className="pt-5 w-full h-[400px] bg-white shadow-md rounded-lg p-4 mb-4 ">
           <h2>{blog.title}</h2>
           <p>{blog.excerpt}</p>
           <a href={`/blog/${blog.slug}`}>Read more</a>
         </div>
+        </div>
       ))}
     </div>
+
+
+
+
+
+
+    </>
   );
 }2
